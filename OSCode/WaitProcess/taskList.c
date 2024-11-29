b80ebb6010f304b5fe820b7a45994a51966fb0eb
Jiasty <2739142322@qq.com>
2024-11-29 17:03:43 +0800

    wait no hang test

---
+++ OSCode/WaitProcess/taskList.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"taskList.h"
+
+void task1()
+{
+    printf("This is the first task!\n");
+}
+
+void task2()
+{
+    printf("This is the second task!\n");
+}
+
+void task3()
+{
+    printf("This is the third task!\n");
+}
